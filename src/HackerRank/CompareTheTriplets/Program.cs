94641e517f2b494e21df3cb96506ae5b513cbd3b
Enzo Thomazi Lucas <enzotlucas31@gmail.com>
2022-08-04 01:32:55 -0300

    Add problem CompareTheTriplets

---
+++ src/HackerRank/CompareTheTriplets/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using CompareTheTriplets;
+
+var listA = new List<int>() { 12, 15, 16};
+
+var listB = new List<int>() { 20, 12, 22};
+
+var response = Solution.Resolve(listA, listB);
+
+Console.WriteLine($"The final score is: {string.Join(" ", response)}. Press any button to shut down...");
+
+Console.ReadKey();
---
+++ src/HackerRank/CompareTheTriplets/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace CompareTheTriplets
+{
+    public static class Solution
+    {
+        public static List<int> Resolve(List<int> a, List<int> b)
+        {
+            if (a is null || b is null | a.Count != b.Count)
+                throw new ArgumentException("Invalid lists.");
+
+            int pointsForA = 0, pointsForB = 0;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] > b[i])
+                    pointsForA++;
+
+                if (b[i] > a[i])
+                    pointsForB++;
+            }
+
+            return new List<int>() { pointsForA, pointsForB };
+        }
+    }
+}
